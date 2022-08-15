db36065912028d4a623bc0c6702d18d53e569ca6
Andrei T <Andrew47795mail.ru>
2022-08-15 16:20:52 +0300

    исправлена задача19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,7 +6,7 @@ string number = Console.ReadLine();
 
 void CheckingNumber(string number)
 {
-    if (number[0] == number[4] || number[1] == number[3])
+    if (number[0] == number[4] && number[1] == number[3])
     {
         Console.WriteLine("палиндром");
     }
